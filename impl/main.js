af56c2c06c11fdbe11e4ba6ffbc8689aa49c2ac5
drdrej <andrej.siebert@googlemail.com>
2014-07-01 13:09:40 +0200

    working on rendering and templates

--- impl/main.js
+++ impl/main.js
@@ -11,7 +11,7 @@ exports.exec = function( config ) {
 
     var json = require( 'json-tools' );
 
-    json.query( config )
+    json.json( config )
         .transform( function( element ) {
             var output = _.template( "<%= $processDir %>/<%= $path %>/<%= name %>.<%= type %>", {
                 $processDir : process.cwd(),
@@ -27,8 +27,75 @@ exports.exec = function( config ) {
                 output : output,
                 src: element
             };
-        });
+        })
 
-    TODO: hier weiter machen: transformation etc... aktuell ist nicht klar, was mit den daten passiert.
+        /* build fields ::: */
+        .transform( function(element){
+            element.fields = [];
+
+            var selectable = json.selectable( element );
+            selectable.each( ".src > .model > .fields", function( fieldDef ) {
+                console.log( "-- build field: %j", fieldDef );
+                var field = fieldDef.render( "private final <%= text( '.type' ) %>  <%= text( '.name' ) %>;");
+
+                element.fields.push(field);
+            });
+
+            return element;
+        })
+
+        .transform( function( element ) {
+            element.methods = [];
+
+            json.selectable( element )
+                .each( ".src > .model > .fields",
+                    function( fieldDef ) {
+                      var field = fieldDef.render( 'file://../templates/GetterMethod.java.tmpl');
+                      element.methods.push(field);
+                });
+
+            return element;
+        })
+        .transform( function(element) {
+            return element;
+        })
+        .render( "./<%= output %>" );
 
 };
+
+
+/*
+TODO: hier weiter machen: transformation etc... aktuell ist nicht klar, was mit den daten passiert.
+
+
+
+{
+    "version" : 1,
+
+    "type" : "java",
+
+    "artefact" : "EnumObjectPattern",
+
+    "name"     : "TestEnum",
+
+    "output"   : "{{project.src}}/java/main/src/{{path}}/{{name}}.{{type}}",
+
+    "model"    : {
+
+    "imports" : [ "java.lang.String" ],
+
+        "fields"  : [
+        {
+            "name" : "_id",
+            "type" : "String"
+        }
+    ],
+
+        "values" : [
+        {
+            "_id" : "XYZ"
+        }
+    ]
+}
+}
+    */
