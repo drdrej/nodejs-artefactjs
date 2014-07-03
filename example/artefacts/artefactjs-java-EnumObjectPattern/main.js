fe088a63661da0c614c3bb59c90571d3cb435475
drdrej <andrej.siebert@googlemail.com>
2014-07-03 21:03:41 +0200

    addFields transformation

--- example/artefacts/artefactjs-java-EnumObjectPattern/main.js
+++ example/artefacts/artefactjs-java-EnumObjectPattern/main.js
@@ -11,12 +11,15 @@ exports.exec = function( config, options) {
             };
         })
         .asSelectable()
+        .transform( 'file://' + __dirname + '/transform/addCtxPaths' )
 
         .transform( 'file://' + __dirname + '/transform/setClassName' )
         .transform( 'file://' + __dirname + '/transform/setFileType' )
         .transform( 'file://' + __dirname + '/transform/setOutput' )
-        .transform( 'file://' + __dirname + '/transform/addCtxPaths' )
+        .transform( 'file://' + __dirname + '/transform/addFields' )
 
         .dump( 'file://' + __dirname + '/test.json' );
+
+         // .render()
 };
 
---
+++ example/artefacts/artefactjs-java-EnumObjectPattern/transform/addFields.js
@@ -0,0 +1,15 @@
+
+exports.transform = function( model, options ) {
+    model.fields = [];
+
+    model.each( ".$src > .$json >.model > .fields", function( fieldDef ) {
+        console.log( "-- build field: %j", fieldDef );
+
+        var fieldTmpl = "private final <%= text( '.type' ) %>  <%= text( '.name' ) %>;";
+        var field = fieldDef.render( fieldTmpl );
+
+        model.fields.push(field);
+    });
+
+    return model;
+};
